92e2b994a21ebac2c3635f631c9fa76405734c2d
Brant Smith <brant.lee.smith88@gmail.com>
2022-04-06 07:21:17 -0400

    Updated cron. Added README.

--- utils/cronJobs.js
+++ utils/cronJobs.js
@@ -3,11 +3,11 @@ const wordpicker = require("./wordPicker");
 const knex = require("knex")(require("../knex_db/knexfile"));
 
 exports.randomWordPickerJob = schedule.scheduleJob(
-  "9 5 * * *",
+  "1 0 * * *",
   wordpicker.wordPicker
 );
 
-exports.gameDayIncrementorJob = schedule.scheduleJob("9 5 * * *", () => {
+exports.gameDayIncrementorJob = schedule.scheduleJob("1 0 * * *", () => {
   knex("teams")
     .select("current_game_day")
     .limit(1)
